7f130a5ef78b03f4c2226ba05a83a21278db3567
thamaraiselvi2005 <thamaraiselvik.ai2023@dce.edu.in>
2026-07-08 18:37:08 +0530

    Time: 23 ms (45.85%) | Memory: 20.2 MB (85.32%) - LeetSync

---
+++ 55-jump-game/jump-game.py
@@ -0,0 +1,13 @@
+class Solution:
+    def canJump(self, nums):
+
+        maxReach = 0
+
+        for i in range(len(nums)):
+
+            if i > maxReach:
+                return False
+
+            maxReach = max(maxReach, i + nums[i])
+
+        return True
