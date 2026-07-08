ee52d7ff5e748e38596e9dbd7e2e2bb572105213
thamaraiselvi2005 <thamaraiselvik.ai2023@dce.edu.in>
2026-07-08 18:29:29 +0530

    Time: 3 ms (32.03%) | Memory: 19.7 MB (9.54%) - LeetSync

---
+++ 46-permutations/permutations.py
@@ -0,0 +1,27 @@
+class Solution:
+    def permute(self, nums):
+
+        result = []
+
+        def backtrack(path, used):
+
+            if len(path) == len(nums):
+                result.append(path[:])
+                return
+
+            for i in range(len(nums)):
+
+                if used[i]:
+                    continue
+
+                used[i] = True
+                path.append(nums[i])
+
+                backtrack(path, used)
+
+                path.pop()
+                used[i] = False
+
+        backtrack([], [False] * len(nums))
+
+        return result
