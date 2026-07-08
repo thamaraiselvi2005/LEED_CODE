059e5a498eccb6fe3c389987a6f3561ee8413616
thamaraiselvi2005 <thamaraiselvik.ai2023@dce.edu.in>
2026-07-08 18:27:30 +0530

    Time: 0 ms (100.00%) | Memory: 19.3 MB (65.26%) - LeetSync

---
+++ 20-valid-parentheses/valid-parentheses.py
@@ -0,0 +1,23 @@
+class Solution:
+    def isValid(self, s):
+
+        stack = []
+
+        bracket = {
+            ')': '(',
+            '}': '{',
+            ']': '['
+        }
+
+        for char in s:
+
+            if char in bracket:
+                if stack and stack[-1] == bracket[char]:
+                    stack.pop()
+                else:
+                    return False
+
+            else:
+                stack.append(char)
+
+        return len(stack) == 0
